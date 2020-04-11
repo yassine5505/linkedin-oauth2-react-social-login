8c2dd36317b5576e4ab77c78b204c8a311c09e64
Mohammed Yassine Chraibi <med.yassine@beebay.co>
2020-04-11 17:15:20 +0100

    Corrected typo in config file

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export default LinkedInApi = {
     clientId: 'XXXXXXXXXX',
-    redirectUrl: 'https://www.your-sire.com/linkedin',
+    redirectUrl: 'https://www.your-site.com/linkedin',
     oauthUrl: 'https://www.linkedin.com/oauth/v2/authorization?response_type=code',
     scope: 'r_liteprofile%20r_emailaddress'
 };
