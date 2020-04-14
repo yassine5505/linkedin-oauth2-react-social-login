8cb1d84694ae4e0c8ba841cb62f0e5a9c27855ee
Mohammed Yassine Chraibi <med.yassine@beebay.co>
2020-04-14 21:11:58 +0100

    Getting linkedin access tokengit status

--- backend/app.js
+++ backend/app.js
@@ -2,17 +2,56 @@ const express = require('express')
 const 
 const app = express()
 const axios = require('axios');
-const LinkedInApp = require('./config.js');
+
+const urlToGetLinkedInAccessToken = 'https://www.linkedin.com/oauth/v2/accessToken';
 
 app.get('/login', function (req, res) {
   var code = req.body.code;
-  // Making request to get access token
-  
-  // Making a second request to get user image, first and last names
-
-  // Making a third one to get email address 
-  // (since it requires a different scope)
+  if(getAccessToken(code) === null) break;
 })
+
+/**
+ * Get access token from LinkedIn
+ * @param {*} code returned from step 1
+ * @returns accessToken if successful or null if request fails 
+ */
+function getAccessToken(code) {
+  let accessToken = null;
+  const config = {
+    headers: { "Content-Type": 'application/x-www-form-urlencoded' }
+  };
+  const parameters = {
+    "grant_type": "authorization_code",
+    "code": code,
+    "redirect_uri": process.env.REDIRECT_URL,
+    "client_id": process.env.CLIENT_ID,
+    "client_secret": process.env.CLIENT_SECRET,
+  };
+
+  axios
+    .post(
+      urlToGetLinkedInAccessToken,
+      qs.stringify(parameters),
+      config)
+    .then(response => {
+      accessToken = response.data["access_token"];
+    })
+    .catch(err => {
+      console.log("Error getting LinkedIn access token:");
+      console.log(err.body);
+    })
+    return accessToken;
+}
+
+function getUserInformation(accessToken) {
+  // Make request to get user's full name and profile image URL
+}
+
+function getUserEmailAddress(accessToken) {
+  // Make request to get user's email address
+}
+
+
 
 app.listen(3000, function () {
   console.log(`Node server running...`)
