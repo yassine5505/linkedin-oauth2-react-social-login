319b3b41aa38ec453510a36621f67aed8de73e1c
Mohammed Yassine Chraibi <med.yassine@beebay.co>
2020-04-15 21:12:56 +0100

    Refactoring frontend code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,94 @@
 import React from 'react';
 import{ LinkedInApi, NodeServer } from './config';
 import axios from 'axios';
-import linkedInLoginImage from './linkedin-login-images/Retina/Sign-In-Large---Default.png';
-
-const getCodeFromWindowURL = url => {
-  const popupWindowURL = new URL(url);
-  return popupWindowURL.searchParams.get("code");
-};
+import linkedInLoginImage from './linkedin-login-images/Retina/Sign-In-Small---Default.png';
 
 export default class App extends React.Component {
-    componentDidMount = () => {
-      if (window.opener && window.opener !== window) {
-        const code = getCodeFromWindowURL(window.location.href);
-        window.opener.postMessage({'type': 'code', 'code': code}, '*')
-        window.close();
-      }
-        window.addEventListener('message', this.handlePostMessage);
-    };
+  initialState = {
+    user: {},
+    loggedIn: false
+  };
 
-    handlePostMessage = event => {
-        if (event.data.type === 'code') {
-          const { code } = event.data;
-          this.getUserCredentials(code);
-        }
-    };
+  constructor(props) {
+    super(props);
+    this.state = this.initialState;
+  }
 
-    getUserCredentials = code => {
-      axios
-        .get(NodeServer.baseURL + NodeServer.getUserCredentials)
-        .then(res => {
-            // Do something with data
-            console.log(res.data);
-        })
+  componentDidMount = () => {
+    if (window.opener && window.opener !== window) {
+      const code = this.getCodeFromWindowURL(window.location.href);
+      window.opener.postMessage({'type': 'code', 'code': code}, '*')
+      window.close();
     }
+      window.addEventListener('message', this.handlePostMessage);
+  };
 
-    showPopup = () => {
-        const clientId = LinkedInApi.clientId;
-        const redirectUrl = LinkedInApi.redirectUrl;
-        const oauthUrl = `${LinkedInApi.oauthUrl}&client_id=${clientId}&scope=${LinkedInApi.scope}&state=123456&redirect_uri=${redirectUrl}`;
-        const width = 450,
-          height = 730,
-          left = window.screen.width / 2 - width / 2,
-          top = window.screen.height / 2 - height / 2;
-        window.open(
-          oauthUrl,
-          'Linkedin',
-          'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' +
-          width +
-          ', height=' +
-          height +
-          ', top=' +
-          top +
-          ', left=' +
-          left
-        );
-    };
+  handlePostMessage = event => {
+    if (event.data.type === 'code') {
+      const { code } = event.data;
+      this.getUserCredentials(code);
+    }
+  };
 
-    render() {
-        return (
-            <div>
-                <h2>Sign in with LinkedIn</h2>
-                <img src={linkedInLoginImage} alt="Sign in with LinkedIn" onClick={this.showPopup} />
-            </div>
-        )
-    };
+  getCodeFromWindowURL = url => {
+    const popupWindowURL = new URL(url);
+    return popupWindowURL.searchParams.get("code");
+  };
+
+  showPopup = () => {
+    const clientId = LinkedInApi.clientId;
+    const redirectUrl = LinkedInApi.redirectUrl;
+    const oauthUrl = `${LinkedInApi.oauthUrl}&client_id=${clientId}&scope=${LinkedInApi.scope}&state=123456&redirect_uri=${redirectUrl}`;
+    const width = 450,
+      height = 730,
+      left = window.screen.width / 2 - width / 2,
+      top = window.screen.height / 2 - height / 2;
+    window.open(
+      oauthUrl,
+      'Linkedin',
+      'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' +
+      width +
+      ', height=' +
+      height +
+      ', top=' +
+      top +
+      ', left=' +
+      left
+    );
+  };
+
+  getUserCredentials = code => {
+    axios
+      .get(NodeServer.baseURL + NodeServer.getUserCredentials)
+      .then(res => {
+        const user = res.data;
+        this.setState({
+          user,
+          loaded: true
+        })
+        // Do something with user
+      });
+  };
+
+  render() {
+    const { loggedIn, user } = this.state;
+    const contentWhenLoggedIn = (
+      <>
+        <img src={user.profileImageURL} alt="Profile image" />
+        <h3>{`${user.firstName} ${user.lastName}`}</h3>
+        <h3>{user.email}</h3>
+      </>
+    );
+    const contentWhenLoggedOut = (
+        <>
+          <h2>Sign in with LinkedIn</h2>
+          <img src={linkedInLoginImage} alt="Sign in with LinkedIn"onClick={this.showPopup} />
+        </>
+    );
+    return (
+      <div>
+        {loggedIn ? contentWhenLoggedIn : contentWhenLoggedOut}
+      </div>
+    )
+  };
 }
